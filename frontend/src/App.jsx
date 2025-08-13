6aa1ca55456df945d8b470445b1fe83dbb1653cd
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-08-13 17:14:57 +0530

    addiing more functionality in our app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,35 +5,31 @@ import "./App.css";
 import { Button } from "./components/ui/button";
 import Navbar from "./components/Navbar";
 import { Textarea } from "./components/ui/textarea";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+
+
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element:<Login/>
+  },{
+    
+    path:"/home",
+    element:<Home/>
+  }
+])
+
+
+
+
 
 function App() {
-  const [title, settitle] = useState("");
-  const [description, setDescrition] = useState(" ");
-
-  const handleChange = () => {
-    console.log(title, description);
-  };
+  
   return (
     <div className="">
-      <Navbar />
-
-      <div className="flex items-center  w-full mt-4">
-        <div className="flex flex-col">
-          <input
-            value={title}
-            onChange={(e) => settitle(e.target.value)}
-            placeholder=" enter your todo..."
-            className="border flex-1 mb-2"
-          />
-          <Textarea
-            value={description}
-            onChange={(e) => setDescrition(e.target.value)}
-          />
-        </div>
-        <Button onClick={handleChange} className="ml-4">
-          hello world
-        </Button>
-      </div>
+    <RouterProvider router={appRouter}/>
     </div>
   );
 }
